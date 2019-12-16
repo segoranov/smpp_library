668359aa94a2f2a48b1293431a31b8921dbecc0c
Stiliyan Goranov <stiliyan.goranov@opencode.com>
2019-12-16 17:18:27 +0200

    Update basebind class

--- smpp/pdu/basebind.h
+++ smpp/pdu/basebind.h
@@ -3,8 +3,8 @@
 
 #include <string>
 
-#include "pdu_request.h"
 #include "../type/address.h"
+#include "pdu_request.h"
 
 namespace smpp {
 
@@ -14,10 +14,22 @@ class BaseBind : public PduRequest {
   std::string m_strPassword;
   std::string m_strSystemType;
   uint8_t m_nInterfaceVersion;
-  Address address;
+  Address m_address;
 
  public:
   explicit BaseBind(uint32_t nCommandId);
+
+  std::string getSystemId() const { return m_strSystemId; }
+  std::string getPassword() const { return m_strPassword; }
+  std::string getSystemType() const { return m_strSystemType; }
+  uint8_t getInterfaceVersion() const { return m_nInterfaceVersion; }
+  Address getAddress() const { return m_address; }
+
+  void setSystemId(const std::string& strSystemId){m_strSystemId = strSystemId; }
+  void setSystemType(const std::string& strSystemType) { m_strSystemType = strSystemType; }
+  void setPassword(const std::string& strPassword) { m_strPassword = strPassword; }
+  void setInterfaceVersion(uint8_t nInterfaceVersion) { m_nInterfaceVersion = nInterfaceVersion; }
+  void setAddress(const Address& newAddress) { m_address = newAddress; }
 };
 
 }  // namespace smpp
